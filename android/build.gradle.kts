e914003e841db4f3275224145e3c417c23cb997b
Su Ho <su.v@nimblehq.co>
2022-08-24 17:12:00 +0700

    [#2] Setup CI/CD for iOS

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,20 +1,58 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("com.android.application")
     kotlin("android")
 }
 
+val keystoreProperties = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "signing.properties")))
+}
+
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file("../config/release.keystore")
+            storePassword = keystoreProperties.getProperty("KEYSTORE_PASSWORD")
+            keyPassword = keystoreProperties.getProperty("KEY_PASSWORD")
+            keyAlias = keystoreProperties.getProperty("KEY_ALIAS")
+        }
+
+        getByName("debug") {
+            storeFile = file("../config/debug.keystore")
+            storePassword = "oQ4mL1jY2uX7wD8q"
+            keyAlias = "debug-key-alias"
+            keyPassword = "oQ4mL1jY2uX7wD8q"
+        }
+    }
+
     compileSdk = 32
     defaultConfig {
         applicationId = "co.nimblehq.ic.kmm.suv.android"
         minSdk = 23
         targetSdk = 32
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs["release"]
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs["debug"]
+        }
+    }
+    flavorDimensions += "version"
+    productFlavors {
+        create("staging") {
+            applicationIdSuffix = ".staging"
+        }
+        create("production") {
         }
     }
 }
--- ios/Surveys/Sources/Application/Application.swift
+++ ios/Surveys/Sources/Application/Application.swift
@@ -1,7 +1,12 @@
+// swiftlint:disable weak_delegate
+import FirebaseCore
 import SwiftUI
 
 @main
 struct Application: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    var delegate: AppDelegate
 
     var body: some Scene {
         WindowGroup {
