b57cf820ff6399399b77df39370317f99eacae18
Su Ho <su.v@nimblehq.co>
2022-11-04 10:34:05 +0700

    [#21] Create UI for the home header on iOS

---
+++ ios/Surveys/Sources/Presentation/Modules/Home/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  Surveys
+//
+//  Created by Su Ho on 03/11/2022.
+//  Copyright © 2022 Nimble. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    var body: some View {
+        VStack {
+            HomeHeaderView(
+                title: "Monday, JUNE 16",
+                imageURLString: "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png"
+            )
+            .padding(EdgeInsets(top: 0.0, leading: 20.0, bottom: 0.0, trailing: 20.0))
+            Spacer()
+        }
+        .background(Color.black) // Remove this later
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        HomeView()
+    }
+}
