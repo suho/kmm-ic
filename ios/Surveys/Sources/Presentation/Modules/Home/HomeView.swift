f5be0bfb013df999ca82e57223bb96abeb4e0a6b
Su Ho <su.v@nimblehq.co>
2022-11-09 10:00:37 +0700

    [#21] Add todo comments

--- ios/Surveys/Sources/Presentation/Modules/Home/HomeView.swift
+++ ios/Surveys/Sources/Presentation/Modules/Home/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
 
     var body: some View {
         VStack {
+            // TODO: - Demo data for POW
             HomeHeaderView(
                 title: "Monday, JUNE 16",
                 imageURLString: "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png"
@@ -19,7 +20,7 @@ struct HomeView: View {
             .padding(EdgeInsets(top: 0.0, leading: 20.0, bottom: 0.0, trailing: 20.0))
             Spacer()
         }
-        .background(Color.black) // Remove this later
+        .background(Color.black) // TODO: - Remove this after implement surveys list feature
         .preferredColorScheme(.dark)
     }
 }
