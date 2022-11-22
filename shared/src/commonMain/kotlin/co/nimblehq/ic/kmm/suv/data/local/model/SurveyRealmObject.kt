894c617d6153500da5bf9c8d13d2785f02b5a236
Su Ho <su.v@nimblehq.co>
2022-11-22 11:21:54 +0700

    [#33] Improve constructor of realm object

--- shared/src/commonMain/kotlin/co/nimblehq/ic/kmm/suv/data/local/model/SurveyRealmObject.kt
+++ shared/src/commonMain/kotlin/co/nimblehq/ic/kmm/suv/data/local/model/SurveyRealmObject.kt
@@ -4,15 +4,34 @@ import co.nimblehq.ic.kmm.suv.domain.model.Survey
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 
-open class SurveyRealmObject(
+open class SurveyRealmObject : RealmObject {
+
     @PrimaryKey
-    var id: String = "",
-    var type: String = "",
-    var title: String = "",
-    var description: String = "",
-    var isActive: Boolean = false,
+    var id: String = ""
+    var type: String = ""
+    var title: String = ""
+    var description: String = ""
+    var isActive: Boolean = false
     var coverImageUrl: String = ""
-) : RealmObject
+
+    constructor(
+        id: String,
+        type: String,
+        title: String,
+        description: String,
+        isActive: Boolean,
+        coverImageUrl: String
+    ) : this() {
+        this.id = id
+        this.type = type
+        this.title = title
+        this.description = description
+        this.isActive = isActive
+        this.coverImageUrl = coverImageUrl
+    }
+
+    constructor()
+}
 
 fun SurveyRealmObject.toSurvey() = Survey(
     title,
